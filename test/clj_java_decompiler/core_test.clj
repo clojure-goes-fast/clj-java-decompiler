1ca3034ad40cc5e1a02d75d5b7b8cd738dfc44d2
Alexander Yakushev <alex@bytopia.org>
2022-07-30 14:24:08 +0300

    Add tests and CircleCI

---
+++ test/clj_java_decompiler/core_test.clj
@@ -0,0 +1,104 @@
+(ns clj-java-decompiler.core-test
+  (:require [clj-java-decompiler.core :as sut]
+            [clojure.test :refer :all]
+            clojure.pprint
+            [clojure.string :as str]))
+
+(defn ns-fixture
+  [f]
+  (in-ns 'clj-java-decompiler.core-test)
+  (f))
+
+;; Force tests to be run within this namespaces
+;; (see https://github.com/cognitect-labs/test-runner/issues/38)
+(use-fixtures :once ns-fixture)
+
+(defmacro with-out-trimmed-str [& body]
+  `(let [res# (with-out-str ~@body)]
+     (->> res#
+          str/split-lines
+          (remove str/blank?)
+          (str/join "\n"))))
+
+(defn =str [s1 s2]
+  (let [s1s (str/split-lines s1)
+        s2s (str/split-lines s2)]
+    (or (every? true? (map (fn [l1 l2]
+                             (or (= l1 "<<<anything>>>")
+                                 (= l2 "<<<anything>>>")
+                                 (= l1 l2)
+                                 (when-let [[_ before after] (re-matches #"(.*)<<<ignore>>>(.*)" l2)]
+                                   (and (str/starts-with? l1 before)
+                                        (str/ends-with? l1 after)))))
+                           (str/split-lines s1)
+                           (str/split-lines s2)))
+        (do (println "FAIL\ns1:")
+            (clojure.pprint/pprint s1s)
+            (println  "\n-------\ns2:\n")
+            (clojure.pprint/pprint s2s)
+            false))))
+
+(deftest basic-test
+  (is (=str (with-out-trimmed-str
+              (sut/decompile
+               (loop [i 100, sum 0]
+                 (if (< i 0)
+                   sum
+                   (recur (unchecked-dec i) (unchecked-add sum i))))))
+            "<<<anything>>>
+package clj_java_decompiler;
+import clojure.lang.*;
+<<<anything>>>
+{
+    public static Object invokeStatic() {
+        long i = 100L;
+        long sum = 0L;
+        while (i >= 0L) {
+            final long n = i - 1L;
+            sum += i;
+            i = n;
+        }
+        return Numbers.num(sum);
+    }
+    @Override
+    public Object invoke() {
+        return invokeStatic();
+    }
+}"))
+
+  (=str (with-out-trimmed-str
+          (sut/decompile (defn hello [] (println "Hello, decompiler!"))))
+        "// Decompiling class: clj_java_decompiler/core_test$hello
+package clj_java_decompiler;
+import clojure.lang.*;
+public final class core_test$hello extends AFunction
+{
+    public static final Var const__0;
+    public static Object invokeStatic() {
+        return ((IFn)core_test$hello.const__0.getRawRoot()).invoke(\"Hello, decompiler!\");
+    }
+    @Override
+    public Object invoke() {
+        return invokeStatic();
+    }
+    static {
+        const__0 = RT.var(\"clojure.core\", \"println\");
+    }
+}"))
+
+(deftest lambda-linenumber-test
+  (is (=str (with-out-trimmed-str
+              (sut/decompile (fn [] (+ 1 2))))
+            "// Decompiling class: clj_java_decompiler/core_test$fn_line_91__<<<ignore>>>
+package clj_java_decompiler;
+import clojure.lang.*;
+public final class core_test$fn_line_91__<<<ignore>>> extends AFunction
+{
+    public static Object invokeStatic() {
+        return Numbers.num(Numbers.add(1L, 2L));
+    }
+    @Override
+    public Object invoke() {
+        return invokeStatic();
+    }
+}")))
